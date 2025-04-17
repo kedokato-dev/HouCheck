e87ce68b3fdf67bb5306402711b1d7325b48c648
Trần Anh Quân <thocodeanhquan@gmail.com>
2025-04-17 22:43:25 +0700

    test

--- app/src/main/java/com/kedokato_dev/houcheck/navhost/NavHost.kt
+++ app/src/main/java/com/kedokato_dev/houcheck/navhost/NavHost.kt
@@ -2,12 +2,9 @@ package com.kedokato_dev.houcheck.navhost
 
 
 import androidx.compose.runtime.Composable
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import com.kedokato_dev.houcheck.ui.LoginScreen
 import com.kedokato_dev.houcheck.ui.view.HomeScreen
 
--- app/src/main/java/com/kedokato_dev/houcheck/ui/view/LoginScreen.kt
+++ app/src/main/java/com/kedokato_dev/houcheck/ui/view/LoginScreen.kt
@@ -1,14 +1,24 @@
 package com.kedokato_dev.houcheck.ui
 
 import android.widget.Toast
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -47,6 +57,9 @@ fun LoginScreen(navHostController: NavHostController) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+
+        Text("Login", style = androidx.compose.material3.MaterialTheme.typography.headlineLarge)
+
         OutlinedTextField(
             value = username,
             onValueChange = { username = it },
